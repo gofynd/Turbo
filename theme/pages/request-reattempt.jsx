90e3cb5ef13279df19d45743b925e0a77ba95581
Jigar Dafda <jigar.dafda@gmail.com>
2025-11-27 10:44:25 +0000

    [Auto Generated]

--- theme/pages/request-reattempt.jsx
+++ theme/pages/request-reattempt.jsx
@@ -571,16 +571,13 @@ const RequestReattempt = ({ fpi }) => {
     };
   };
 
-  function convertISOToDDMMYYYY(isoString) {
-    if (!isoString) return "";
-
-    const date = new Date(isoString);
-    if (isNaN(date)) return "";
-
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-
+ function convertISOToDDMMYYYY(utcString) {
+    if (!utcString) return "";
+    // Extract only the date part before 'T'
+    const [datePart] = utcString.split("T");
+    if (!datePart) return "";
+    const [year, month, day] = datePart.split("-");
+    if (!year || !month || !day) return "";
     return `${day}-${month}-${year}`;
   }
 
